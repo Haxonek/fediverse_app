d38c20c6f4b36db0035b0a134579a1c44f744857
Haxonek <thomash1996@live.com>
2019-11-07 09:11:39 -0700

    Re-added original menu items in addition to new ones

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is a fork of electron-quick-start which is being built into a Fediverse des
 
 **It is not currently finished and has no easy installation**
 
-At the moment this is an exceptionally simple project which will accept an instance and will render it as a website. More functionality to come. There's no reason this shouldn't be compatible with all your favorite Fediverse instances.
+At the moment this is an exceptionally simple project which will accept an instance and will render it as a website. There is a menu bar above which you can use to jump back to the selection page (Home) or to other popular instances. More functionality to come. There's no reason this shouldn't be compatible with all your favorite Fediverse instances.
 
 You can learn more about [the Fediverse here](https://en.wikipedia.org/wiki/Fediverse) and you can learn about the most popular instances statistics [here](https://fediverse.network/). Additionally here is a cool graphic about some of the instances [here](https://fediverse.party/).
 
--- main.js
+++ main.js
@@ -17,6 +17,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 900,
     height: 600,
+    title: "Fediverse",
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
@@ -34,33 +35,140 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+        mainWindow = null
   })
 
-  // add menu
-  menu = Menu.buildFromTemplate([
-      {
-          label: 'Instances',
+  var template = [
+        {
+          label: 'Fediverse',
           submenu: [
-              {label:'Home', click() {mainWindow.loadFile('index.html')}},
-              {type:'separator'},
-              {label:'Mastodon', click() {rebuildWindow('https://mastodon.social/')}},
-              {label:'Pawoo', click() {rebuildWindow('https://pawoo.net/')}},
-              {label:'Mstdn', click() {rebuildWindow('https://mastdn.social/')}},
-              {label:'Socialhome', click() {rebuildWindow('https://socialhome.network/')}},
-              {label:'Pixelfed', click() {rebuildWindow('https://pixelfed.org/')}},
-              {label:'Gab', click() {rebuildWindow('https://gab.com/')}},
-              {type:'separator'},
               {
-                  label:'Quit',
-                  click() {
-                      app.quit()
-                  }
-              }
-          ]
-      }
-  ])
-  Menu.setApplicationMenu(menu);
+                  label: 'About Electron',
+                  selector: 'orderFrontStandardAboutPanel:'
+              },
+              {
+                  type: 'separator'
+              },
+              {
+                  label: 'Services',
+                  submenu: []
+              },
+              {
+                  type: 'separator'
+              },
+              {
+                  label: 'Hide',
+                  accelerator: 'Command+H',
+                  selector: 'hide:'
+              },
+              {
+                  label: 'Hide Others',
+                  accelerator: 'Command+Shift+H',
+                  selector: 'hideOtherApplications:'
+              },
+              {
+                  label: 'Show All',
+                  selector: 'unhideAllApplications:'
+              },
+              {
+                  type: 'separator'
+              },
+              {
+                  label: 'Quit',
+                  accelerator: 'Command+Q',
+                  click: function() { app.quit(); }
+              },
+          ]},
+          {
+              label: 'Instances',
+              submenu: [
+                  {label:'Home', click() {mainWindow.loadFile('index.html')}},
+                  {type:'separator'},
+                  {label:'Mastodon', click() {rebuildWindow('https://mastodon.social/')}},
+                  {label:'Pawoo', click() {rebuildWindow('https://pawoo.net/')}},
+                  {label:'Mstdn', click() {rebuildWindow('https://mastdn.social/')}},
+                  {label:'Socialhome', click() {rebuildWindow('https://socialhome.network/')}},
+                  {label:'Pixelfed', click() {rebuildWindow('https://pixelfed.org/')}},
+                  {label:'Gab', click() {rebuildWindow('https://gab.com/')}},
+                  {type:'separator'},
+              ]
+          },
+          {
+              label: 'Edit',
+              submenu: [
+                  {
+                      label: 'Undo',
+                      accelerator: 'Command+Z',
+                      selector: 'undo:'
+                  },
+                  {
+                      label: 'Redo',
+                      accelerator: 'Shift+Command+Z',
+                      selector: 'redo:'
+                  },
+                  {
+                      type: 'separator'
+                  },
+                  {
+                      label: 'Cut',
+                      accelerator: 'Command+X',
+                      selector: 'cut:'
+                  },
+                  {
+                      label: 'Copy',
+                      accelerator: 'Command+C',
+                      selector: 'copy:'
+                  },
+                  {
+                      label: 'Paste',
+                      accelerator: 'Command+V',
+                      selector: 'paste:'
+                  },
+                  {
+                      label: 'Select All',
+                      accelerator: 'Command+A',
+                      selector: 'selectAll:'
+                  },
+              ]},
+              {
+                  label: 'View',
+                  submenu: [
+                      {
+                          label: 'Reload',
+                          accelerator: 'Command+R',
+                          click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); }
+                      },
+                  ]
+              },
+              {
+                  label: 'Window',
+                  submenu: [
+                      {
+                          label: 'Minimize',
+                          accelerator: 'Command+M',
+                          selector: 'performMiniaturize:'
+                      },
+                      {
+                          label: 'Close',
+                          accelerator: 'Command+W',
+                          selector: 'performClose:'
+                      },
+                      {
+                          type: 'separator'
+                      },
+                      {
+                          label: 'Bring All to Front',
+                          selector: 'arrangeInFront:'
+                      },
+                  ]
+              },
+              {
+                  label: 'Help',
+                  submenu: []
+              },
+          ];
+    menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
 }
 
 // set up web instance
@@ -77,11 +185,11 @@ function rebuildWindow(instanceURL) {
     console.log(instanceURL)
     mainWindow.close() // there must be a better way to do this lol
     mainWindow = new BrowserWindow({
-      width: 900,
-      height: 600,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
+        width: 900,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     mainWindow.loadURL(instanceURL)
@@ -94,15 +202,15 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
